5ce6ecc72e42b7f6484743eeef361b24cc74c7b5
crmgunter <crmgunter@gmail.com>
2019-06-25 16:31:27 -0400

    more adapter stuff

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Home'
 import WebcamMain from './WebcamMain';
 import ReactWebcam from './ReactWebcam'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import adapter from 'webrtc-adapter';
 
 function App() {
   return (
