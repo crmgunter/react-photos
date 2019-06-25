fd3a8ce213160047cd4579ad5936a5e6dbed7eec
crmgunter <crmgunter@gmail.com>
2019-06-25 15:31:40 -0400

    added video constraints for user facing camera

--- src/ReactWebcam.js
+++ src/ReactWebcam.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import Webcam from 'react-webcam';
 class ReactWebcam extends Component {
     render() {
+        const videoConstraints = {
+            facingMode: "user"
+          };
+
         return (
             <div>
-                <Webcam />
+                <Webcam videoConstraints={videoConstraints}/>
             </div>
         );
     }
